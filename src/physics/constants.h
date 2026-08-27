6697a11e5c4477182fa0d189bdf6dd961ecdb375
IfanPrasetyoAji <ifan.prasetyo.aji@gmail.com>
2026-08-27 18:05:17 +0700

    chore: rescale sim constants

--- src/physics/constants.h
+++ src/physics/constants.h
@@ -1,9 +1,26 @@
 #pragma once
 
-// Normally G is 6.6743e-11 but spacebox use
-// this value instead, to scale with the distance unit used.
-// (1 Distance Unit = 5e6KM)
-#define GRAVITATIONAL_CONSTANT 7.9275e-3f
+// Sim unit
+#define DISTANCE_UNIT 5.0e6f
+#define DISTANCE_UNIT_METERS 5.0e9f
+
+#define SOLAR_MASS 1.989e30f
+#define MASS_UNIT_KILOGRAMS (SOLAR_MASS) // 1 mass unit = 1 solar mass
+
+#define TIME_UNIT 1.0f
+#define TIME_UNIT_SECONDS 86400.0f // 1 time unit = 1 day
+
+// Real life constants
+#define G_REAL 6.67430e-11f
+#define C_REAL 299792458.0f
+
+// Sim constants
+// G_SIM = G_REAL * MASS_UNIT_KILOGRAMS * DISANCE_UNIT_METERS^2 /
+// (TIME_UNIT_SECONDS^3)
+#define G 7.9279f
+
+// C_SIM = C_REAL * TIME_UNIT_SECONDS / DISTANCE_UNIT_METERS
+#define C 5180.41f
 
 #define EARTH_PERIOD_UNIT 365.0f
 #define SECONDS_PER_PERIOD 30.0f
